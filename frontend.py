0f00fd10c7f486f07d563afeffbc3f7420f0532b
KarolisTam <130557721+KarolisTam@users.noreply.github.com>
2023-05-16 14:41:06 +0300

    Ištaisytos klaidos ir sujungti forntend

--- frontend.py
+++ frontend.py
@@ -1,64 +1,69 @@
 import PySimpleGUI as sg
+from backend import Product, engine
+from sqlalchemy.orm import sessionmaker
 
+session = sessionmaker(bind=engine)()
 
 
 class BookshopGUI():
 
-    def get_data(self):
-        self.book_list = session.query(Books).all()
-        data = [[item.id, item.book_title, item.author, item.year, item.price, item.quantity]
-            for item in self.book_list]
-        return data
-
+    def get_product_list(self):
+        self.products = session.query(Product).all()
+        product_list = []
+        for item in self.products:           
+            product_list.append([item.id, item.book_title, item.author, item.year, item.price, item.quantity])
+        return product_list
 
     def __init__(self):
-            headers = ['ID', 'Book Title', "Author", 'Year of Release', 'Price', 'Quantity']
-            self.table = sg.Table(values=, headings=headers, auto_size_columns=True, key="-TABLE-", enable_events=True)
-            self.layout = [
-                [self.table],
-                [sg.Button ("ADD TO CART",), sg.Button("VIEW CART"), sg.Button("FILTER BOOKS BY AUTHOR"), sg.Button("FILTER BOOKS BY THE YEAR"), sg.Button("EXIT"), sg.Button("VIEW PURCHASE HISTORY")]]
-            self.window = sg.Window("BOOK_SHOP", layout=self.layout)
-
-
+        headers = ['ID', 'Book Title', "Author", 'Year of Release', 'Price', 'Quantity']
+        self.table = sg.Table(values=self.get_product_list(), headings=headers, 
+                              auto_size_columns=True, key="-TABLE-", enable_events=True)
+        self.layout = [
+            [self.table],
+            [sg.Button("ADD TO CART"), 
+             sg.Button("VIEW CART"), 
+             sg.Button("FILTER BOOKS BY AUTHOR"), 
+             sg.Button("FILTER BOOKS BY THE YEAR"), 
+             sg.Button("EXIT"), 
+             sg.Button("VIEW PURCHASE HISTORY")]]
+        self.window = sg.Window("BOOK_SHOP", layout=self.layout)
 
     def run(self):
-            while True:
-                event, value = self.window.read
-                if event == sg.WINDOW_CLOSED or event == 'EXIT':
-                    break
-                elif event == 'ADD TO CART':
-                    pass
-                elif event == 'VIEW CART':
-                    pass
-                elif event == 'FILTER BOOKS BY AUTHOR':
-                    pass
-                elif event == 'FILTER BOOKS BY YEAR':
-                    pass
-                elif event == 'VIEW PURCHASE HISTORY':
-                    pass
+        while True:
+            event, values = self.window.read()
+            if event == sg.WINDOW_CLOSED or event == 'EXIT':
+                break
+            elif event == 'ADD TO CART':
+                pass
+            elif event == 'VIEW CART':
+                pass
+            elif event == 'FILTER BOOKS BY AUTHOR':
+                pass
+            elif event == 'FILTER BOOKS BY YEAR':
+                pass
+            elif event == 'VIEW PURCHASE HISTORY':
+                pass
 
 
-def shopping_cart():
+def shopping_oder():
+
     sg.theme("LightGreen5")
     headers =["Book name", "Author", "Release year", "Price"]
     layout =[
-        [sg.Table(values="", headings=headers, auto_size_columns=True, key="cart_table", enable_events=True)],
-        [sg.Button("Remove", key="remove"), sg.Button("Purchase", key="purchase"), sg.Button("Close Shopping Cart", key="close")]
+        [sg.Table(values="", headings=headers, auto_size_columns=True, key="order_table", enable_events=True)],
+        [sg.Button("Remove", key="remove"), sg.Button("Purchase", key="purchase"), sg.Button("Close Shopping Order", key="close")]
     ]
-    shopcart = sg.Window("Shoping Cart", layout)
+    shopcart = sg.Window("Shoping Order", layout)
     while True:
         event, values = shopcart.read()
         if event in (sg.WIN_CLOSED, 'close'):
             break
         elif event == "remove":
-            selected_rows = values["cart_table"][0]
+            selected_rows = values["order_table"][0]
             print(selected_rows)
 
    
     shopcart.close()
-
-import PySimpleGUI as sg
-
 
 def purchase_history():
     sg.theme("LightGreen5")
@@ -75,4 +80,4 @@ def purchase_history():
    
     history.close()
 
-shopping_cart()
+
