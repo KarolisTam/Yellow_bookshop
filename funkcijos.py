b9b2fa7cef1e0670398f4d0b7480918273404be3
Kobrius11 <kobrius11@gmail.com>
2023-05-17 10:24:50 +0300

    Fixed login class, some minor fixes

--- funkcijos.py
+++ funkcijos.py
@@ -30,3 +30,4 @@ class LentelesFunkcijos:
 #LentelesFunkcijos(Status).set_element(1, name='name2', name2='name3')
 #LentelesFunkcijos(SecurityQuestions).add_element(question_text='Pirmo augintinio vardas ?')
 #LentelesFunkcijos(Product).add_element(book_name='', author='', release_date='', price= ,)
+#LentelesFunkcijos(Customer).set_element(1, security_key='GreenDay')
